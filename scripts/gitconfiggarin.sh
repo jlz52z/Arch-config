a50965ec1a8f1eb2f8175da7fa560950609ba7e6
jlz52z <yuqing09101223@outlook.com>
2025-05-24 18:07:56 +0800

    something

---
+++ scripts/gitconfiggarin.sh
@@ -0,0 +1,4 @@
+git config user.name "garinyanjoy"
+git config user.email "garinyanjoy@gmail.com"
+git config user.signingkey 466B1FB2C96B3234E79EE5CB1EB3C99A3BC4CC4B
+
---
+++ scripts/gitconfigjlz52z.sh
@@ -0,0 +1,4 @@
+git config user.name "jlz52z"
+git config user.email "yuqing09101223@outlook.com"
+git config user.signingkey BA0B3A96D9792E908EF3578AB9E58C56CA3577A4
+
