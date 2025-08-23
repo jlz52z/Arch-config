b0af76fd0cca71fc3a391551fab09d23ea23acd9
jlz52z <yuqing09101223@outlook.com>
2025-08-23 15:15:31 +0800

    something

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,6 +1,7 @@
 return {
 	"nvimtools/none-ls.nvim",
 	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
@@ -13,6 +14,22 @@ return {
 				-- json 格式化
 				null_ls.builtins.formatting.prettier,
 			},
+
+			-- you can reuse a shared lspconfig on_attach callback here
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							vim.lsp.buf.format({async = false})
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
